4c9c61576a656a0b8e426ffef0ec00c8e1b65a77
Su13n <nestler.jeanmaurice@gmail.com>
2024-12-25 20:38:33 +0000

    Removed PIL package

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from more_itertools import sliced
 import discord
 from discord import app_commands
 from discord.ext import commands
-from PIL import Image
 from io import BytesIO
 import requests
 import re
