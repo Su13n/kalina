15a194dac6676b320be13b2c31c8850af9676cdb
Su13n <nestler.jeanmaurice@gmail.com>
2024-12-30 12:09:25 +0100

    refactor name comparison

--- main.py
+++ main.py
@@ -124,13 +124,13 @@ async def embed_create(interaction: discord.Interaction, doll: str):
             print(f"Normalised: {normalized}\nOrig. input: {doll}\nCN: {canonical_name}\n\n")
             base_embed = gf2_embeds.get_embed(canonical_name)
             images = DOLL_IMAGES[canonical_name]
-            continue
+            break
 
     # If not in dictionary, respond ephemeral
     if not base_embed:
         await interaction.response.send_message("There's no doll with that name!", ephemeral=True)
     return
-
+    print("so far so good")
     # if normalized in DOLL_NAMES["makiatto"]:
     #     base_embed = gf2_embeds.get_makiatto()
     #     images = DOLL_IMAGES["makiatto"]
